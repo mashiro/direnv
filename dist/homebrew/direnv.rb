1dce0011491ee9736391d438e627d9fa96b17a96
zimbatm <zimbatm@zimbatm.com>
2013-11-17 04:31:21 +0000

    Homebrew / OSX dist

---
+++ dist/homebrew/direnv.rb
@@ -0,0 +1,24 @@
+require 'formula'
+
+class Direnv < Formula
+  homepage 'http://www.direnv.net'
+  url 'https://github.com/zimbatm/direnv/archive/v2.1.0.tar.gz'
+  sha1 'e2f88d6fc9f6a55bd0b377f6dd46c1d2278fffe0'
+
+  head 'https://github.com/zimbatm/direnv.git'
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "install", "DESTDIR=#{prefix}"
+  end
+
+  def caveats; <<-EOS.undent
+    At the END of your ~/.bashrc or ~/.zshrc, add the following line:
+      eval "$(direnv hook $0)"
+
+    See the wiki for docs and examples:
+      https://github.com/zimbatm/direnv/wiki/
+    EOS
+  end
+end
